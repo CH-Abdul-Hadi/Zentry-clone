e786ad951497c93bf2cc01310df85be13c092e4e
CH-Abdul-Hadi <adbulhadi502@gmail.com>
2025-04-18 12:13:13 +0500

    Features complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Navbar from "./Components/Navbar";
+import Features from "./Components/Features";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero />
       <About />
+      <Features/>
     </main>
   );
 }
